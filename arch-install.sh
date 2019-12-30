182c3e056539bf127076e437c8a0bf168888d1e5
DomestosInDaHood <ahharris@tutanota.com>
2019-12-30 12:09:32 +0200

    Проверка загружаемого режима

---
+++ arch-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Необходим для вывода кирилицы в консоль
+setfont cyr-sun16
+
+# Проверка загружаемого режима
+if ls /sys/firmware/efi/efivars
+then 
+	bootMode = "efi"
+	echo "Archiso загружен в режиме EFI"
+else 
+	bootMode = "bios"
+	echo "Archiso загружен в режиме BIOS"
